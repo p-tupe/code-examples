9ef3df976829ab55dde69236401fa715f9dd1eaa
Pritesh Tupe <mail@priteshtupe.com>
2025-11-29 20:25:30 -0500

    Add part 2 of AoC2024 go soln

--- go/AoC2024/Historian_Hysteria/main.go
+++ go/AoC2024/Historian_Hysteria/main.go
@@ -8,33 +8,23 @@ import (
 )
 
 func main() {
-	first, second, err := ParseIP(ip)
+	first, second, err := parseIP(ip)
 	if err != nil {
 		panic(err)
 	}
 
-	slices.Sort(first)
-	slices.Sort(second)
+	distance := calcDist(first, second)
+	fmt.Println("Distance:", distance)
 
-	distance := 0
-	for i, f := range first {
-		s := second[i]
-		d := f - s
-		if d > 0 {
-			distance += d
-		} else {
-			distance -= d
-		}
-	}
-
-	fmt.Println("Distance: ", distance)
+	similarity := calcSim(first, second)
+	fmt.Println("Similarity:", similarity)
 }
 
-// ParseIP takes in the raw string input and
+// parseIP takes in the raw string input and
 // returns the 2 slices of location ids.
 // It returns an error if a number cannot be parsed,
 // or it the arrays aren't of same length
-func ParseIP(str string) ([]int, []int, error) {
+func parseIP(str string) ([]int, []int, error) {
 	first := make([]int, 0, len(str)/2)
 	second := make([]int, 0, len(str)/2)
 
@@ -55,4 +45,44 @@ func ParseIP(str string) ([]int, []int, error) {
 	}
 
 	return first, second, nil
+}
+
+// calcDist takes in 2 slices of ints, sorts them
+// in asc order, and sums the difference between
+// each pair at same index.
+func calcDist(first []int, second []int) int {
+	slices.Sort(first)
+	slices.Sort(second)
+
+	distance := 0
+	for i, f := range first {
+		s := second[i]
+		d := f - s
+		if d > 0 {
+			distance += d
+		} else {
+			distance -= d
+		}
+	}
+
+	return distance
+}
+
+// calcSim takes in 2 slices of ints, and for each number
+// in first list, calculates the frequency at which it appears
+// in second list; similarity = sum of all | number * frequency
+func calcSim(first []int, second []int) int {
+	sim := 0
+
+	for _, f := range first {
+		count := 0
+		for _, s := range second {
+			if f == s {
+				count++
+			}
+		}
+		sim += f * count
+	}
+
+	return sim
 }
